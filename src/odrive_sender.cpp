1110f156f41161c9ea7d5b9a1caefea91fd0c489
CaoQDdon <caoquangddon5@gmail.com>
2024-10-29 16:43:36 +0700

    change Byte to ByteMultiArray

--- src/odrive_sender.cpp
+++ src/odrive_sender.cpp
@@ -6,19 +6,19 @@
 class odriveSender : public rclcpp::Node
 {
 private:
-  void topic_callback(const std_msgs::msg::Byte & msg) const
+  void topic_callback(const std_msgs::msg::ByteMultiArray & msg) const
   {
     /*
     Code here if you trust me unconditional
     */
   }
-  rclcpp::Subscription<std_msgs::msg::Byte>::SharedPtr subscription_;
+  rclcpp::Subscription<std_msgs::msg::ByteMultiArray>::SharedPtr subscription_;
 
 public:
   odriveSender()
   : Node("odrive_sender")
   {
-    subscription_ = this->create_subscription<std_msgs::msg::Byte>(
+    subscription_ = this->create_subscription<std_msgs::msg::ByteMultiArray>(
       "topic", 10, std::bind(&odriveSender::topic_callback, this, _1));
   }
 };
